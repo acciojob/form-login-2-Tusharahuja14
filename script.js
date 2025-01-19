d4950942c22cc4a99f8f06a7a092bbf1fbb05fb1
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-01-19 13:45:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let fname=document.getElementById("firstname");
+let lname=document.getElementById("lastname");
+let pnum=document.getElementById("phonenumber");
+let emailid=document.getElementById("emailid");
+function show()
+{
+	alert(`First Name:${fname.value} Last Name:${lname.value} Phone Number:${pnum.value} Email ID:${emailid.value}`)
+}
